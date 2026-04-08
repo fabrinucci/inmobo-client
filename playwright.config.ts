749f4008dd457c9d6f5af32e55797bc61a367ec7
Fabrizio Nucci <fabrinucci99@gmail.com>
2026-04-08 18:37:41 +0000

    chore: simplify Playwright configuration by removing unused browser projects

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,11 +13,7 @@ export default defineConfig({
     trace: 'on-first-retry',
   },
 
-  projects: [
-    { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
-    { name: 'firefox', use: { ...devices['Desktop Firefox'] } },
-    { name: 'webkit', use: { ...devices['Desktop Safari'] } },
-  ],
+  projects: [{ name: 'chromium', use: { ...devices['Desktop Chrome'] } }],
 
   webServer: {
     command: 'npm run start',
